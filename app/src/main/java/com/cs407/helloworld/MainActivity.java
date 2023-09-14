b9ef0a275ba4bd6fbdb2b1dc64d8f0776312a544
Vienna <vazhang@wisc.edu>
2023-09-14 13:52:36 -0500

    Update

---
+++ app/src/main/java/com/cs407/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cs407.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
